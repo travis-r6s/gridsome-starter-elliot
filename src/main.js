5f4c44ee5a0d3c3aa3f774ec6ebb0ff3a68d9e32
Travis Reynolds <travis@cirruscreative.co.uk>
2020-05-15 22:56:13 +0000

    Import Styles

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
+// Layouts
 import DefaultLayout from '~/layouts/Default.vue'
 
-export default function (Vue, { router, head, isClient }) {
+// Styles
+import '@storefront-ui/vue/styles.scss'
+import '@/styles/storefront.scss'
+import '@/styles/base.scss'
+
+export default function (Vue) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
