72a747375033d6f81af5818803b27290cf99d9ea
Travis Reynolds <travis@cirruscreative.co.uk>
2020-05-16 15:35:10 +0000

    Add demo footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,52 @@
+<template>
+  <SfFooter
+    :column="column"
+    :multiple="multiple">
+    <SfFooterColumn
+      v-for="column in columns"
+      :key="column.title"
+      :title="column.title">
+      <SfList v-if="column.items">
+        <SfListItem
+          v-for="item in column.items"
+          :key="item">
+          <SfMenuItem :label="item" />
+        </SfListItem>
+      </SfList>
+    </SfFooterColumn>
+  </SfFooter>
+</template>
+
+<script>
+// Components
+import { SfFooter, SfList, SfImage, SfMenuItem } from '@storefront-ui/vue'
+export default {
+  name: 'TheFooter',
+  components: {
+    SfFooter,
+    SfList,
+    SfImage,
+    SfMenuItem
+  },
+  data: () => ({
+    isMobile: false,
+    columns: [
+      {
+        title: 'About us',
+        items: ['Who we are', 'Quality in the details', 'Customer Reviews']
+      },
+      {
+        title: 'Departments',
+        items: ['Women fashion', 'Men fashion', 'Kidswear', 'Home']
+      },
+      {
+        title: 'Help',
+        items: ['Customer service', 'Size guide', 'Contact us']
+      },
+      { title: 'Payment & delivery', items: ['Purchase terms', 'Guarantee'] }
+    ],
+    column: 4,
+    multiple: false
+  })
+}
+</script>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,15 +2,17 @@
   <div class="layout">
     <TheHeader />
     <slot />
+    <TheFooter />
   </div>
 </template>
 
 <script>
 // Components
 import TheHeader from '@/components/TheHeader'
+import TheFooter from '@/components/TheFooter'
 
 export default {
   name: 'MainLayout',
-  components: { TheHeader }
+  components: { TheHeader, TheFooter }
 }
 </script>
