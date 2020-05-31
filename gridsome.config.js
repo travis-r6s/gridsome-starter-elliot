6ba9e64f7618be5e8d2a048adb5222b4ed0bf609
Travis Reynolds <travis@cirruscreative.co.uk>
2020-05-31 00:19:16 +0000

    Add flexsearch plugin config

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,6 +7,27 @@ module.exports = {
         keys: process.env.ELLIOT_ENV,
         logs: true
       }
+    },
+    {
+      use: 'gridsome-plugin-flexsearch',
+      options: {
+        flexsearch: {
+          profile: 'match'
+        },
+        searchFields: ['name'],
+        collections: [
+          {
+            typeName: 'Product',
+            indexName: 'Product',
+            fields: ['title', 'id', 'slug', 'description', 'images']
+          },
+          {
+            typeName: 'Collection',
+            indexName: 'Collection',
+            fields: ['title', 'id', 'slug']
+          }
+        ]
+      }
     }
   ]
 }
